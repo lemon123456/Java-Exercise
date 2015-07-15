a26309db0ce47310b265faa69dd654ee5b97238b
lemon123456 <xiaojuanxu@yahoo.com>
2015-07-15 16:59:17 +0800

    First release

--- core/src/main/java/com/tw/core/controller/UserController.java
+++ core/src/main/java/com/tw/core/controller/UserController.java
@@ -71,7 +71,7 @@ public class UserController {
 //                }
 
                 CookieUtil.deleteCookie(request,response);
-                
+
                 return new ModelAndView("redirect:" + previousURL);
             }else{
                 return new ModelAndView("redirect:"+"/users");
